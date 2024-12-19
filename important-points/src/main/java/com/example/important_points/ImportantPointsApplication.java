0ccc957864202728740f900b62cbe5a34204068d
dbengp <dbengp@hotmail.com>
2024-12-19 02:18:12 -0300

    base code, containing only the initial structure that will be used for the entire development of the project.

---
+++ important-points/src/main/java/com/example/important_points/ImportantPointsApplication.java
@@ -0,0 +1,13 @@
+package com.example.important_points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportantPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportantPointsApplication.class, args);
+	}
+
+}
---
+++ important-points/src/test/java/com/example/important_points/ImportantPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.important_points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportantPointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
